3efa212db8861d01485c9bf74a67ad6affaec0ae
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-02 16:41:17 +1200

    loading indicator added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-export { loadingIndicator } from './lib/loading-indicator.js';
+import { loadingIndicator } from './lib/loading-indicator.js';
+
+export class GeocamViewerLoadingIndicator extends HTMLElement {
+  constructor() {
+    super();
+    this.plugin = null;
+    // this.yaw = this.getAttribute('yaw') || 0;
+    console.log("loading-indicator init");
+  }
+
+  connectedCallback() {
+    console.log("loading-indicator connected");
+    const node = this;
+    const parent = this.parentNode;
+    if (parent.viewer && parent.viewer.plugin) {
+      // Call a method on the parent
+        this.plugin = new loadingIndicator();
+  parent.viewer.plugin(this.plugin);
+    } else {
+      console.error(
+        "GeocamViewerLocadingIndicator must be a child of GeocamViewer"
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    this.plugin = null;
+    console.log("loading-indicator disconnected");
+    // Clean up the viewer
+  }
+}
+
+window.customElements.define(
+  "geocam-viewer-loading-indicator",
+  GeocamViewerLoadingIndicator
+);
